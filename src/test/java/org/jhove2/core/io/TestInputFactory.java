a8caf1ba97e6b0523468c076443bf1e058208395
mstrong <mstrong@507d08b6-3e80-4f80-935c-3d33354f88f8>
2009-07-10 22:29:08 +0000

    tests URLSource

--- src/test/java/org/jhove2/core/io/TestInputFactory.java
+++ src/test/java/org/jhove2/core/io/TestInputFactory.java
@@ -1,13 +1,21 @@
 package org.jhove2.core.io;
 
+import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.zip.ZipException;
+import java.util.zip.ZipFile;
 
+import org.jhove2.core.JHOVE2;
+import org.jhove2.core.JHOVE2Exception;
 import org.jhove2.core.io.Input.Type;
+import org.jhove2.core.source.URLSource;
+import org.jhove2.core.source.ZipFileSource;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Ignore;
@@ -22,10 +30,35 @@ public class TestInputFactory {
 	@After
 	public void tearDown() throws Exception {
 	}
-	@Ignore
 	@Test
-	public void testGetInputInputStreamIntTypeByteOrder() {
-		fail("Not yet implemented"); // TODO
+	public void testGetInputInputStreamIntType() 
+		throws JHOVE2Exception 
+	{
+		URL yahoo = null;
+		try {
+			yahoo = new URL("http://www.yahoo.com/");
+		} catch (MalformedURLException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		try {
+			JHOVE2 jhove2 = new JHOVE2();
+			URLSource yahooURL = new URLSource(jhove2, yahoo);
+			Input input = yahooURL.getInput(8192, Type.Direct);
+			// this closes the channel and the stream associated with any temp file created
+			input.close();
+			File file = input.getFile();
+			assertTrue("File doesn't exist", file.exists());
+			// this invokes file.delete() on any temp file created
+			yahooURL.close();
+			file = yahooURL.getFile();
+			assertTrue("File still exists - should be deleted!", !file.exists());
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
+	
 
 }
