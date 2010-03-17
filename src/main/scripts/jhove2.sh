b089c1c1c1f011afc9746b51f6c026092489899a
mstrong <mstrong@507d08b6-3e80-4f80-935c-3d33354f88f8>
2010-03-17 22:33:47 +0000

    Minor updates to change version number from 1.9.5 to 0.6.0

--- src/main/scripts/jhove2.sh
+++ src/main/scripts/jhove2.sh
@@ -26,7 +26,12 @@
 # NOTE: Edit the following line to refer to the proper Java command
 #	This MUST be Java 1.6 or greater.
 
-JAVA=/usr/bin/java
+if [ ${JAVA_HOME} ] ; then
+  JAVA=$JAVA_HOME/bin/java
+else
+  JAVA=java
+fi
+
 PRG=$0
 
 # need this for relative symlinks
@@ -46,7 +51,7 @@ JHOVE2_HOME=`dirname $PRG`
 JHOVE2_HOME=`cd $JHOVE2_HOME && pwd`
 
 
-CP=$JHOVE2_HOME/lib/jhove2-1.9.5.jar:$JHOVE2_HOME/lib/aopalliance-1.0.jar:$JHOVE2_HOME/lib/commons-logging-1.1.1.jar:$JHOVE2_HOME/lib/commons-logging-api-1.1.jar:$JHOVE2_HOME/lib/jdom-1.0.jar:$JHOVE2_HOME/lib/junit-4.4.jar:$JHOVE2_HOME/lib/log4j-1.2.14.jar:$JHOVE2_HOME/lib/jargs-1.0.jar:$JHOVE2_HOME/lib/spring-beans-2.5.6.jar:$JHOVE2_HOME/lib/spring-context-2.5.6.jar:$JHOVE2_HOME/lib/spring-core-2.5.6.jar:$JHOVE2_HOME/lib/spring-test-2.5.6.jar:$JHOVE2_HOME/lib/soap-2.3.1.jar:$JHOVE2_HOME/lib/xercesImpl-2.9.1.jar:$JHOVE2_HOME/lib/xml-apis-1.3.04.jar:$JHOVE2_HOME/lib/xml-resolver-1.2.jar:$JHOVE2_HOME/config:$JHOVE2_HOME/config/droid
+CP=$JHOVE2_HOME/lib/jhove2-0.6.0.jar:$JHOVE2_HOME/lib/aopalliance-1.0.jar:$JHOVE2_HOME/lib/commons-logging-1.1.1.jar:$JHOVE2_HOME/lib/commons-logging-api-1.1.jar:$JHOVE2_HOME/lib/jdom-1.0.jar:$JHOVE2_HOME/lib/junit-4.4.jar:$JHOVE2_HOME/lib/log4j-1.2.14.jar:$JHOVE2_HOME/lib/jargs-1.0.jar:$JHOVE2_HOME/lib/spring-beans-2.5.6.jar:$JHOVE2_HOME/lib/spring-context-2.5.6.jar:$JHOVE2_HOME/lib/spring-core-2.5.6.jar:$JHOVE2_HOME/lib/spring-test-2.5.6.jar:$JHOVE2_HOME/lib/soap-2.3.1.jar:$JHOVE2_HOME/lib/xercesImpl-2.9.1.jar:$JHOVE2_HOME/lib/xml-apis-1.3.04.jar:$JHOVE2_HOME/lib/xml-resolver-1.2.jar:$JHOVE2_HOME/config:$JHOVE2_HOME/config/droid
 
 # NOTE: Nothing below this line should be edited
 ############################################################################
