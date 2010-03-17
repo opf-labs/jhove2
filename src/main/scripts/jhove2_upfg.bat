985f19c805ddf5c80ad78c7932268f0f2274fb7c
mstrong <mstrong@507d08b6-3e80-4f80-935c-3d33354f88f8>
2010-03-17 07:14:45 +0000

    updated bat files to determine JHOVE2_HOME and JAVA definitions automagically.

--- src/main/scripts/jhove2_upfg.bat
+++ src/main/scripts/jhove2_upfg.bat
@@ -14,8 +14,19 @@ REM NOTE: Edit the following lines to refer to the proper file system paths
 REM       of the JHOVE2 installation directory and the Java command
 REM
 
-SET JHOVE2_HOME="C:\Program Files\jhove2"
-SET JAVA="C:\Program Files\Java\jdk1.6.0_14\bin\java"
+REM
+REM If JAVA_HOME is not set, C:/WINDOWS/System32/java.exe is used
+REM
+if "%JAVA_HOME%" == "" (
+   set JAVA=java
+) else (
+   set JAVA="%JAVA_HOME%\bin\java"
+)
+SET JHOVE2_HOME=%~dp0
+
+REM ECHO JAVA = %JAVA%
+REM ECHO JAVA_HOME = %JAVA_HOME%
+REM ECHO JHOVE2_HOME = %JHOVE2_HOME%
 
 SET CP=%JHOVE2_HOME%\lib\jhove2-1.9.5.jar;%JHOVE2_HOME%\lib\aopalliance-1.0.jar;%JHOVE2_HOME%\lib\commons-logging-1.1.1.jar;%JHOVE2_HOME%\lib\commons-logging-api-1.1.jar;%JHOVE2_HOME%\lib\jdom-1.0.jar;%JHOVE2_HOME%\lib\junit-4.4.jar;%JHOVE2_HOME%\lib\log4j-1.2.14.jar;%JHOVE2_HOME%\lib\jargs-1.0.jar;%JHOVE2_HOME%\lib\spring-beans-2.5.6.jar;%JHOVE2_HOME%\lib\spring-context-2.5.6.jar;%JHOVE2_HOME%\lib\spring-core-2.5.6.jar;%JHOVE2_HOME%\lib\spring-test-2.5.6.jar;%JHOVE2_HOME%\lib\soap-2.3.1.jar;%JHOVE2_HOME%\lib\xercesImpl-2.9.1.jar;%JHOVE2_HOME%\lib\xml-apis-1.3.04.jar;%JHOVE2_HOME%\lib\xml-resolver-1.2.jar;%JHOVE2_HOME%\config;%JHOVE2_HOME%\config\droid
 
