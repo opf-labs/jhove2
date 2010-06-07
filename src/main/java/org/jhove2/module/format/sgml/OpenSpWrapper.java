f80d3e63e8bd38f62a13957a3e0d070569519ab6
smmorrissey Shelia Morrissey <sheila.morrissey@ithaka.org>
2010-06-07 09:36:09 -0400

    Add utility classes and associated config.xml and unit test for invoking an external shell

--- src/main/java/org/jhove2/module/format/sgml/OpenSpWrapper.java
+++ src/main/java/org/jhove2/module/format/sgml/OpenSpWrapper.java
@@ -36,7 +36,9 @@
 package org.jhove2.module.format.sgml;
 
 
+import org.jhove2.annotation.ReportableProperty;
 import org.jhove2.core.JHOVE2Exception;
+import org.jhove2.util.externalprocess.ExternalProcessHandler;
 
 /**
  * Wrapper around OpenSP SGML parser and onsgmlNorm utility. 
@@ -56,7 +58,36 @@ import org.jhove2.core.JHOVE2Exception;
  *
  */
 public class OpenSpWrapper {
-
+    
+    /** parser directive: path to catalog file; if empty or null, no catalog is assumed */
+    protected String catalogPath;
+    
+    /** parser directive: max number of errors detected after which parser halts; default is 200; 0 means unlimited
+        if < 0, will use onsgmls default */
+    protected int maxErrors = -1; 
+    
+    /** parser directive: Show open entities in error messages */
+    protected boolean showOpenEntitiesInErrMsg;  
+    
+    /** parser directive: Show open elements in error messages */
+    protected boolean showOpenElementsInErrMsg;
+    
+    /** parser directive: include error number in error messages */
+    protected boolean showErrNumberInErrMsg;
+    
+    protected ExternalProcessHandler processHandler;
+    
+ //   /usr/bin/onsgmls -E0 -gnxl -wall -oentity -ocomment -oid -oincluded -ononsgml -onotation-sysid -oomitted -odata-attribute -c$CATALOG -f$SGMERR $SGMIN > $SGMOUT
+    
+	/**
+	 * Invokes onsmgls processor to parse and validate the SGML
+	 * source.  Then invokes ANTLR-generated parser to accumulate
+	 * feature information about the instance
+	 * @param sgm SgmlModule instance invoking this method; module will have 
+	 *            Source memeber
+	 * @return ANTLR-generated parser with accumulated feature information
+	 * @throws JHOVE2Exception
+	 */
 	public ESISCommandsParser parseFile(SgmlModule sgm)
 	throws JHOVE2Exception {
 		String esisFilePath = null;
@@ -80,4 +111,104 @@ public class OpenSpWrapper {
 		
 		return docType;		
 	}
+	
+
+	/**
+	 * @return the catalogPath
+	 */
+	@ReportableProperty(order = 20, value = "Parser setting:  Full path to catalog file")
+	public String getCatalogPath() {
+		return catalogPath;
+	}
+
+
+	/**
+	 * @param catalogPath the catalogPath to set
+	 */
+	public void setCatalogPath(String catalogPath) {
+		this.catalogPath = catalogPath;
+	}
+
+
+	/**
+	 * @return the maxErrors
+	 */
+	@ReportableProperty(order = 21, value = "Parser setting:  Error limit before abandoning parse")
+	public int getMaxErrors() {
+		return maxErrors;
+	}
+
+
+	/**
+	 * @param maxErrors the maxErrors to set
+	 */
+	public void setMaxErrors(int maxErrors) {
+		this.maxErrors = maxErrors;
+	}
+
+
+	/**
+	 * @return the showOpenEntitiesInErrMsg
+	 */
+	@ReportableProperty(order = 22, value = "Parser setting:  Show open entities in error messages")
+	public boolean isShowOpenEntitiesInErrMsg() {
+		return showOpenEntitiesInErrMsg;
+	}
+
+
+	/**
+	 * @param showOpenEntitiesInErrMsg the showOpenEntitiesInErrMsg to set
+	 */
+	public void setShowOpenEntitiesInErrMsg(boolean showOpenEntitiesInErrMsg) {
+		this.showOpenEntitiesInErrMsg = showOpenEntitiesInErrMsg;
+	}
+
+
+	/**
+	 * @return the showOpenElementsInErrMsg
+	 */
+	@ReportableProperty(order = 23, value = "Parser setting:  Show open elements in error messages")
+	public boolean isShowOpenElementsInErrMsg() {
+		return showOpenElementsInErrMsg;
+	}
+
+
+	/**
+	 * @param showOpenElementsInErrMsg the showOpenElementsInErrMsg to set
+	 */
+	public void setShowOpenElementsInErrMsg(boolean showOpenElementsInErrMsg) {
+		this.showOpenElementsInErrMsg = showOpenElementsInErrMsg;
+	}
+
+
+	/**
+	 * @return the showErrNumberInErrMsg
+	 */
+	@ReportableProperty(order = 22, value = "Parser setting:  Show error message number in error messages")
+	public boolean isShowErrNumberInErrMsg() {
+		return showErrNumberInErrMsg;
+	}
+
+
+	/**
+	 * @param showErrNumberInErrMsg the showErrNumberInErrMsg to set
+	 */
+	public void setShowErrNumberInErrMsg(boolean showErrNumberInErrMsg) {
+		this.showErrNumberInErrMsg = showErrNumberInErrMsg;
+	}
+
+	/**
+	 * @return the processHandler
+	 */
+	public ExternalProcessHandler getProcessHandler() {
+		return processHandler;
+	}
+
+	/**
+	 * @param processHandler the processHandler to set
+	 */
+	public void setProcessHandler(ExternalProcessHandler processHandler) {
+		this.processHandler = processHandler;
+	}
+
 }
---
+++ src/test/java/org/jhove2/util/externalprocess/ForkShellHandlerTest.java
@@ -0,0 +1,182 @@
+/**
+ * JHOVE2 - Next-generation architecture for format-aware characterization
+ * <p>
+ * Copyright (c) 2010 by The Regents of the University of California, Ithaka
+ * Harbors, Inc., and The Board of Trustees of the Leland Stanford Junior
+ * University. All rights reserved.
+ * </p>
+ * <p>
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * </p>
+ * <ul>
+ * <li>Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.</li>
+ * <li>Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.</li>
+ * <li>Neither the name of the University of California/California Digital
+ * Library, Ithaka Harbors/Portico, or Stanford University, nor the names of its
+ * contributors may be used to endorse or promote products derived from this
+ * software without specific prior written permission.</li>
+ * </ul>
+ * <p>
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * </p>
+ */
+package org.jhove2.util.externalprocess;
+
+import static org.junit.Assert.*;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+import javax.annotation.Resource;
+
+import org.jhove2.app.util.FeatureConfigurationUtil;
+import org.jhove2.core.JHOVE2Exception;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * @author smorrissey
+ *
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations={"classpath*:**/test-config.xml", 
+"classpath*:**/filepaths-config.xml"})
+public class ForkShellHandlerTest {
+
+	protected ForkShellHandler shellHandler;
+	protected String tempDirBasePath;
+	protected String tempDirPath;
+	protected File tempFile;
+	protected String tempFilePath;
+    protected FilepathFilter filter;
+	public static final String HELLO = "HELLO";
+	public static final String COMMANDBASE = "echo " + HELLO +  " > ";
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+		try {
+			tempDirPath = 
+				FeatureConfigurationUtil.getFilePathFromClasspath(tempDirBasePath, "temp dir");
+		} catch (JHOVE2Exception e1) {
+			fail("Could not create base directory");
+		}
+		tempFile = File.createTempFile("ForkShellHandlerTest", ".txt", 
+				new File(tempDirPath));
+		tempFile.deleteOnExit();
+		tempFilePath = tempFile.getPath();
+		tempFilePath = filter.filter(tempFilePath);
+	}
+
+	/**
+	 * Test method for {@link org.jhove2.util.externalprocess.ForkShellHandler#executeCommand(java.lang.String)}.
+	 */
+	@Test
+	public void testExecuteCommand() {
+		String command = COMMANDBASE + tempFilePath;
+		try {
+			shellHandler.executeCommand(command);
+		} catch (JHOVE2Exception e) {
+			e.printStackTrace();
+			fail("Shell Handler threw exception");
+		}
+		assertTrue(tempFile.exists());
+		long lngth = tempFile.length();
+		assertTrue(lngth>0);
+		BufferedReader reader = null;
+		try {
+			reader = 
+				new BufferedReader(new FileReader(tempFilePath));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			fail("Exception creating Reader on shell output file");
+		}
+		StringBuffer fileContents = new StringBuffer();
+		String input = null;
+		try {
+			input = reader.readLine();
+		} catch (IOException e) {
+			e.printStackTrace();
+			fail("Exception reading Reader on shell output file");
+		}
+		while (input != null){							
+			fileContents.append(input);
+			try {
+				input = reader.readLine();
+			} catch (IOException e) {
+				break;
+			}
+		};
+		String strContents = fileContents.toString();
+		assertEquals(HELLO, strContents);
+	}
+	/**
+	 * @return the shellHandler
+	 */
+	public ForkShellHandler getShellHandler() {
+		return shellHandler;
+	}
+
+	/**
+	 * @param shellHandler the shellHandler to set
+	 */
+	@Resource
+	public void setShellHandler(ForkShellHandler shellHandler) {
+		this.shellHandler = shellHandler;
+	}
+
+	/**
+	 * @return the tempDirBasePath
+	 */
+	public String getTempDirBasePath() {
+		return tempDirBasePath;
+	}
+
+	/**
+	 * @param tempDirBasePath the tempDirBasePath to set
+	 */
+	@Resource
+	public void setTempDirBasePath(String tempDirBasePath) {
+		this.tempDirBasePath = tempDirBasePath;
+	}
+
+	/**
+	 * @return the filter
+	 */
+	public FilepathFilter getFilter() {
+		return filter;
+	}
+
+	/**
+	 * @param filter the filter to set
+	 */
+	@Resource
+	public void setFilter(FilepathFilter filter) {
+		this.filter = filter;
+	}
+
+
+
+}
